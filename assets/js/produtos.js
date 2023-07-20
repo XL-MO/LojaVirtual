e88b3a06c9fe3cdf288a2bbba30afe7e2c046fe7
Gisely Oliveira <giselymoli@gmail.com>
2023-07-20 02:24:26 -0300

    correção modal

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -3,9 +3,67 @@
  *  e carregar os cards correspondentes
  */
 
+// Função para buscar os produtos a partir de um arquivo JSON
+function buscaProdutos() {
+    var requestURL = "assets/data/produtos.json";
+
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        produtos = request.response;
+        carregarCards(); // Após carregar os produtos, chama a função para carregar os cards na página
+    }
+}
+
+// Função para resgatar um produto pelo ID
+function resgataProduto(id) {
+    for (var i = 0; i < produtos.length; i++) {
+        var item = produtos[i];
+        if (item.id == id) {
+            return item;
+        }
+    }
+}
+
+// Função para criar o HTML de um card de produto
+function criarCard(produto) {
+    var card = "<div class=\"col\">";
+    card += "<div class=\"card shadow-sm\">";
+    card += `<img src=\"assets/images/produtos/${produto.categoria}/${produto.id}.jpg\" class=\"card-img-top\">`;
+    card += "<div class=\"card-body text-center\">";
+    card += `<p class=\"card-text\">${produto.nome}</p>`;
+    card += `<p class=\"card-text\"> R$ ${produto.preco}</p>`;
+    card += "<div class=\"align-items-center\">";
+    card += `<button onclick=\"adicionarCarrinho(${produto.id})\" type=\"button\" class=\"btn btn-primary\">Adicionar</button>`;
+    card += "</div>";
+    card += "</div>";
+    card += "</div>";
+    card += "</div>";
+    return card;
+}
+// Função para criar o HTML de um card de item do carrinho
+function criarCardCarrinho(item) {
+    var card = "<div class=\"col\">";
+    card += "<div class=\"card shadow-sm\">";
+    card += `<img src=\"assets/images/produtos/${item.produto.categoria}/${item.produto.id}.jpg\" class=\"card-img-top\">`;
+    card += "<div class=\"card-body text-center\">";
+    card += `<p class=\"card-text\">${item.produto.nome}</p>`;
+    card += `<p class=\"card-text\"> R$ ${item.produto.preco}</p>`;
+    card += `<p class=\"card-text\"> Quantidade: ${item.quantidade}</p>`;
+    card += "<div class=\"align-items-center\">";
+    card += `<button onclick=\"removerCarrinho(${item.produto.id})\" type=\"button\" class=\"btn btn-danger btn-sm\">Remover</button>`;
+    card += "</div>";
+    card += "</div>";
+    card += "</div>";
+    card += "</div>";
+    return card;
+}
+
 function selecionarMenu(el, categoria) {
     var menus = document.querySelectorAll(".nav-link");
-    for(var i = 0; i<menus.length; i++){                 // Remove os atributos e classes dos menus anteriores
+    for (var i = 0; i < menus.length; i++) {                 // Remove os atributos e classes dos menus anteriores
         var menu = menus[i];
         menu.removeAttribute("aria-current");
         menu.setAttribute('class', 'nav-link link-menu');
@@ -15,74 +73,32 @@ function selecionarMenu(el, categoria) {
 
     carregarCards(categoria);                              // Carrega os cards com base na categoria selecionada 
 }
-
-/*
-// Função para criar o HTML de um card de produto
-*/
-function criarCard(produto) {
-    var card = "<div class=\"col\">";
-        card +=     "<div class=\"card shadow-sm\">";
-        card +=         `<img src=\"assets/images/produtos/${produto.categoria}/${produto.id}.jpg\" class=\"card-img-top\">`;
-        card +=         "<div class=\"card-body text-center\">";
-        card +=             `<p class=\"card-text\">${produto.nome}</p>`;
-        card +=             `<p class=\"card-text\"> R$ ${produto.preco}</p>`;
-        card +=             "<div class=\"align-items-center\">";
-        card +=                 `<button onclick=\"adicionarCarrinho(${produto.id})\" type=\"button\" class=\"btn btn-primary\">Adicionar</button>`;
-        card +=             "</div>";
-        card +=         "</div>";
-        card +=     "</div>";
-        card += "</div>";
-    return card;
-}
-
-// Função para criar o HTML de um card de item do carrinho
-
-function criarCardCarrinho(item) {
-    var card = "<div class=\"col\">";
-        card +=     "<div class=\"card shadow-sm\">";
-        card +=         `<img src=\"assets/images/produtos/${item.produto.categoria}/${item.produto.id}.jpg\" class=\"card-img-top\">`;
-        card +=         "<div class=\"card-body text-center\">";
-        card +=             `<p class=\"card-text\">${item.produto.nome}</p>`;
-        card +=             `<p class=\"card-text\"> R$ ${item.produto.preco}</p>`;
-        card +=             `<p class=\"card-text\"> Quantidade: ${item.quantidade}</p>`;
-        card +=             "<div class=\"align-items-center\">";
-        card +=                 `<button onclick=\"removerCarrinho(${item.produto.id})\" type=\"button\" class=\"btn btn-danger btn-sm\">Remover</button>`;
-        card +=             "</div>";
-        card +=         "</div>";
-        card +=     "</div>";
-        card += "</div>";
-    return card;
-}
-
-var produtos = [];                      // Array para armazenar os produtos
-
-// Função para buscar os produtos a partir de um arquivo JSON
-function buscaProdutos(){
-    var requestURL = "assets/data/produtos.json";
-
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        produtos = request.response;
-        carregarCards();
+// Função para carregar o carrinho na página
+function carregarCarrinho() {
+    var elementopai = document.getElementById("produtos-carrinho");
+    elementopai.innerHTML = "";
+    for (var i = 0; i < itemsCarrinho.length; i++) {
+        var item = itemsCarrinho[i];
+        elementopai.innerHTML += criarCardCarrinho(item);
     }
+    var numItensCarrinho = document.getElementById("num-itens-carrinho");
+    numItensCarrinho.innerHTML = itemsCarrinho.length;
 }
 
-// Função para resgatar um produto pelo ID
+function carregarCardsPesquisa(produtosEncontrados) {
+    selecionarMenu(document.querySelector("#menu-todos"))
 
-function resgataProduto(id) {
-    for(var i = 0; i<produtos.length; i++){
-        var item = produtos[i];
-        if(item.id == id){
-            return item;
-        }
+    var elementopai = document.getElementById("produtos");
+    elementopai.innerHTML = ""; // Limpa o conteúdo do elemento "produtos"
+
+    for (var i = 0; i < produtosEncontrados.length; i++) {
+        var item = produtosEncontrados[i];
+        elementopai.innerHTML += criarCard(item);
     }
 }
 
 /**
- * Carrega os cards com possível filtro por categoria
+ *  Função para carregar os cards com possível filtro por categoria
  * @param {string} categoria 
  */
 function carregarCards(categoria) {
@@ -91,9 +107,9 @@ function carregarCards(categoria) {
 
     var produtosAExibir = [];
     if (categoria) {
-        for(var i = 0; i < produtos.length; i++) {
+        for (var i = 0; i < produtos.length; i++) {
             var item = produtos[i];
-            if(item.categoria == categoria) { //filtra por categoria
+            if (item.categoria == categoria) { //filtra por categoria
                 produtosAExibir.push(item);
             }
         }
@@ -101,31 +117,30 @@ function carregarCards(categoria) {
         produtosAExibir = produtos;
     }
     // Cria os cards para os produtos a serem exibidos
-    for(var i = 0; i<produtosAExibir.length; i++){
+    for (var i = 0; i < produtosAExibir.length; i++) {
         var item = produtosAExibir[i];
         elementopai.innerHTML += criarCard(item);
-    }   
+    }
 }
 
 var itemsCarrinho = [];                     // Array para armazenar os itens do carrinho
 
 
-//Função para adicionar item ao carrinho
-
+// Função para adicionar item ao carrinho
 function adicionarCarrinho(id) {
     var item = resgataProduto(id);
-    if (item){
+    if (item) {
         var existe = false;
-        for (var i=0; i<itemsCarrinho.length; i++){
-            var itemCarrinho=itemsCarrinho[i];
-            if (itemCarrinho.produto.id == id){
+        for (var i = 0; i < itemsCarrinho.length; i++) {
+            var itemCarrinho = itemsCarrinho[i];
+            if (itemCarrinho.produto.id == id) {
                 itemsCarrinho[i].quantidade += 1;
                 existe = true;
                 break;
             }
         }
 
-        if (existe === false){
+        if (existe === false) {
             var itemCarrinho = {
                 "produto": item,
                 "quantidade": 1
@@ -136,19 +151,20 @@ function adicionarCarrinho(id) {
     }
 }
 
+// Função para remover item do carrinho
 function removerCarrinho(id) {
     var item = resgataProduto(id);
-    if (item){
-        for (var i=0; i<itemsCarrinho.length; i++){
-            var itemCarrinho=itemsCarrinho[i];
-            if (itemCarrinho.produto.id == id){
-                if(itemCarrinho.quantidade > 1) {
+    if (item) {
+        for (var i = 0; i < itemsCarrinho.length; i++) {
+            var itemCarrinho = itemsCarrinho[i];
+            if (itemCarrinho.produto.id == id) {
+                if (itemCarrinho.quantidade > 1) {
                     itemsCarrinho[i].quantidade -= 1;
                 } else {
                     itemsCarrinho.splice(i, 1);
                 }
 
-                
+
                 break;
             }
         }
@@ -156,28 +172,16 @@ function removerCarrinho(id) {
     }
 }
 
-
-// Função para carregar o carrinho na página
-
-function carregarCarrinho(){
-    var elementopai = document.getElementById("produtos-carrinho");
-    elementopai.innerHTML = "";
-    for (var i=0; i<itemsCarrinho.length; i++){
-        var item=itemsCarrinho[i];
-        elementopai.innerHTML += criarCardCarrinho(item);
-    }
-    var numItensCarrinho = document.getElementById("num-itens-carrinho");
-    numItensCarrinho.innerHTML = itemsCarrinho.length;
-    
+// Função para calcular o valor total de cada item no carrinho
+function calcularValorItemCarrinho(item) {
+    return item.produto.preco * item.quantidade;
 }
-/**
- * 
- * @param {string|null} criterio 
- */
-function pesquisarProduto(form){
+
+// Função para pesquisar um produto
+function pesquisarProduto(form) {
     var input = form.querySelector('input');
     var termoPesquisa = input.value;
-    if (termoPesquisa){
+    if (termoPesquisa) {
         var produtosEncontrados = [];
         for (var i = 0; i < produtos.length; i++) {
             var produto = produtos[i];
@@ -185,27 +189,47 @@ function pesquisarProduto(form){
                 produtosEncontrados.push(produto);
             }
         }
-        
+
         // Carrega os cards com os produtos encontrados
         carregarCardsPesquisa(produtosEncontrados);
-    }else{
+    } else {
         carregarCards()
     }
 
-   
+
 }
 
-function carregarCardsPesquisa(produtosEncontrados) {
-    selecionarMenu(document.querySelector("#menu-todos"))
 
-    var elementopai = document.getElementById("produtos");
-        elementopai.innerHTML = ""; // Limpa o conteúdo do elemento "produtos"
- 
-    for (var i = 0; i < produtosEncontrados.length; i++) {
-        var item = produtosEncontrados[i];
-        elementopai.innerHTML += criarCard(item);
-    }
-}    
 
 
 window.setTimeout(buscaProdutos, 1000);    //aguarda 1 segundo para buscar produtos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
